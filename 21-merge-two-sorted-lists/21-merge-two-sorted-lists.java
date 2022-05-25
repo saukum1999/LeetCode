b6b84d145dd3be05b3fa155fe2b6a1e644ae68b6
Saurabh Kumar <saukum1999@gmail.com>
2022-05-25 20:27:38 +0530

    Time: 1 ms (65.57%), Space: 43.4 MB (28.36%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -13,7 +13,7 @@ class Solution {
 
         ListNode head = new ListNode();
         ListNode tmp = new ListNode();
-        head = null;
+        head = tmp;
 
         if (l1 == null && l2 == null)
             return null;
@@ -22,42 +22,32 @@ class Solution {
         else if (l2 == null)
             return l1;
 
-        if (l1.val <= l2.val) {
-            tmp = l1;
-            l1 = l1.next;
-        } else {
-            tmp = l2;
-            l2 = l2.next;
-        }
-        head = tmp;
-
         while (l1 != null && l2 != null) {
             if (l2 == null || l1.val <= l2.val) {
                 tmp.next = l1;
                 l1 = l1.next;
                 tmp = tmp.next;
 
-            } else if (l1== null || l1.val > l2.val) {
+            } else if (l1 == null || l1.val > l2.val) {
                 tmp.next = l2;
                 l2 = l2.next;
                 tmp = tmp.next;
             }
         }
-        
-        while( l1 != null){
+
+        while (l1 != null) {
             tmp.next = l1;
-                l1 = l1.next;
-                tmp = tmp.next;
+            l1 = l1.next;
+            tmp = tmp.next;
         }
-        
-        while( l2 != null){
+
+        while (l2 != null) {
             tmp.next = l2;
-                l2 = l2.next;
-                tmp = tmp.next;
+            l2 = l2.next;
+            tmp = tmp.next;
         }
 
-
-        return head;
+        return head.next;
 
     }
 }
