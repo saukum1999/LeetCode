4035b2ea8d8ab2513c83d8cb100d597e9e4a13ab
Saurabh Kumar <saukum1999@gmail.com>
2022-07-31 21:12:30 +0530

    Time: 5 ms (93.52%), Space: 43.4 MB (23.57%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+
+        if (s.length() != t.length())
+            return false;
+
+        int[] map1 = new int[256];
+        int[] map2 = new int[256];
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char a = s.charAt(i);
+            char b = t.charAt(i);
+
+            if( map1[a] == 0 && map2[b]==0){
+                map1[a] = b;
+                map2[b] = a;
+            }
+
+            else if( map1[a] != b && map2[b] != a)
+                return false;
+            
+        }
+
+        return true;
+        
+    }
+}
