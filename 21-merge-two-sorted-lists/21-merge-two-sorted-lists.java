d8323e9a507a58308d225ad7827438e31e4a1b58
Saurabh Kumar <saukum1999@gmail.com>
2022-08-01 21:40:07 +0530

    Time: 1 ms (80.18%), Space: 43.5 MB (16.75%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -8,46 +8,48 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
-        ListNode head = new ListNode();
-        ListNode tmp = new ListNode();
-        head = tmp;
+        ListNode temp = new ListNode();
+        ListNode res = new ListNode();
+        res = temp;
 
         if (l1 == null && l2 == null)
-            return null;
+            return res.next;
         else if (l1 == null)
             return l2;
         else if (l2 == null)
             return l1;
 
         while (l1 != null && l2 != null) {
-            if (l2 == null || l1.val <= l2.val) {
-                tmp.next = l1;
+            if (l1.val <= l2.val) {
+                temp.next = l1;
                 l1 = l1.next;
-                tmp = tmp.next;
-
-            } else if (l1 == null || l1.val > l2.val) {
-                tmp.next = l2;
+                temp = temp.next;
+            } else {
+                temp.next = l2;
                 l2 = l2.next;
-                tmp = tmp.next;
+                temp = temp.next;
             }
         }
 
-        while (l1 != null) {
-            tmp.next = l1;
-            l1 = l1.next;
-            tmp = tmp.next;
+        while (l1 != null || l2 != null) {
+            if (l1 == null) {
+                temp.next = l2;
+                l2 = l2.next;
+                temp = temp.next;
+            } else if (l2 == null) {
+                temp.next = l1;
+                l1 = l1.next;
+                temp = temp.next;
+            }
         }
 
-        while (l2 != null) {
-            tmp.next = l2;
-            l2 = l2.next;
-            tmp = tmp.next;
-        }
-
-        return head.next;
+        return res.next;
 
     }
 }
+
+
