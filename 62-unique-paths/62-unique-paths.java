34daa84fcf7e40e49d408e162198c2059c741283
Saurabh Kumar <saukum1999@gmail.com>
2022-08-10 20:50:47 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (57.39%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,19 +1,27 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int [][] dp = new int[m][n];
-        return count(0,0,m,n,dp);
+        double res = 1;
+        int l = Math.min(m-1,n-1);
+        for(int i = 1; i<=l ;  i++ )
+            res = res * (m + n - 2-(i-1)) / i;
+        return (int) res;
     }
-    public int count(int i, int j, int m, int n, int[][] dp){
-        if(i>=m || j>=n )
-            return 0;
-        else if(i==m-1 && j==n-1){
-            return 1;
-        }
-        if(dp[i][j]!=0)
-            return dp[i][j];
-        else{
-            dp[i][j] = (count(i+1,j,m,n,dp)+count(i,j+1,m,n,dp));
-            return dp[i][j];
-        }
-    }
+    
+    // public int uniquePaths(int m, int n) {
+    //     int [][] dp = new int[m][n];
+    //     return count(0,0,m,n,dp);
+    // }
+    // public int count(int i, int j, int m, int n, int[][] dp){
+    //     if(i>=m || j>=n )
+    //         return 0;
+    //     else if(i==m-1 && j==n-1){
+    //         return 1;
+    //     }
+    //     if(dp[i][j]!=0)
+    //         return dp[i][j];
+    //     else{
+    //         dp[i][j] = (count(i+1,j,m,n,dp)+count(i,j+1,m,n,dp));
+    //         return dp[i][j];
+    //     }
+    // }
 }
