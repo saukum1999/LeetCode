194b94f0190663090ef1de76d5ccd50472080b39
Saurabh Kumar <saukum1999@gmail.com>
2022-08-14 21:49:39 +0530

    Time: 4 ms (23.63%), Space: 41.7 MB (27.02%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -1,27 +1,18 @@
 class Solution {
-    
-    public int nextNum(int n){
-        int res = 0;
-        while(n>0){
-            int a = n%10;
-            n = n/10;
-            res = res + a*a;
-        }
-        return res;
-    }
     public boolean isHappy(int n) {
-        
         HashSet<Integer> set = new HashSet<>();
-        set.add(n);
-        while(n!=1){
-            n = nextNum(n);
-            if(set.contains(n))
+        while (n != 1) {
+            if (set.contains(n))
                 return false;
-            else
-                set.add(n);
+            set.add(n);
+            int res = 0;
+            while (n > 0) {
+                int a = n % 10;
+                n = n / 10;
+                res += a * a;
+            }
+            n = res;
         }
-                    // if(n==1)
-                return true;
-
+        return true;
     }
 }
