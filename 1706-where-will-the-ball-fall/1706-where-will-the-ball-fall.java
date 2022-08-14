22e265b3b7a97c2e57f14f9c956765674f55d15c
Saurabh Kumar <saukum1999@gmail.com>
2022-08-15 00:57:26 +0530

    Time: 1 ms (94.27%), Space: 54.3 MB (48.99%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
@@ -1,53 +1,28 @@
+
+
 class Solution {
-    public int dfs(int[][] grid, int i, int j){
-        if(i==grid.length)
-            return j;
-        
-        if(j<0 || j>=grid[0].length)
-            return -1;
-        
-        if(grid[i][j]==1 && j+1<grid[0].length && grid[i][j+1]==1)
-            return dfs(grid,i+1,j+1);
-        
-        else if(grid[i][j]==-1 && j-1>=0 && grid[i][j-1]==-1)
-            return dfs(grid,i+1,j-1);
-         
-        return -1;
-    }
     public int[] findBall(int[][] grid) {
-        int m = grid[0].length;
-        int[] ar = new int[m];
+
+        int[] list = new int[grid[0].length];
+        for (int i = 0; i < grid[0].length; i++) {
+            list[i]=drop(grid,0,i);
+        }
+        return list;
+    }
+
+    public int drop(int[][] grid, int i, int j) {
+        if (i == grid.length)
+            return j;
+        if (j < 0 ||j >= grid[0].length )
+            return -1;
+
+        if (grid[i][j] == 1 && j+1<grid[0].length && grid[i][j + 1] == 1 ){
+                return drop(grid, i + 1, j + 1);
+        } else if (grid[i][j] == -1 && j-1>=0 &&  grid[i][j - 1] == -1){
+                return drop(grid, i + 1, j - 1);
+        }
         
-        for(int j=0;j<m;j++)
-            ar[j]=dfs(grid,0,j);
-        
-        return ar;
+        return -1;
+
     }
 }
-
-// class Solution {
-//     public int[] findBall(int[][] grid) {
-
-//         int[] list = new int[grid[0].length];
-//         for (int i = 0; i < grid[0].length; i++) {
-//             list[i]=drop(grid,0,i);
-//         }
-//         return list;
-//     }
-
-//     public int drop(int[][] grid, int i, int j) {
-//         if (i == grid.length)
-//             return j;
-//         if (j < 0 ||j >= grid[0].length )
-//             return -1;
-
-//         if (grid[i][j] == 1 && j+1<grid[0].length && grid[i][j + 1] == 1 ){
-//                 return drop(grid, i + 1, j + 1);
-//         } else if (grid[i][j] == -1 && j-1>0 &&  grid[i][j - 1] == -1){
-//                 return drop(grid, i + 1, j - 1);
-//         }
-        
-//         return -1;
-
-//     }
-// }
