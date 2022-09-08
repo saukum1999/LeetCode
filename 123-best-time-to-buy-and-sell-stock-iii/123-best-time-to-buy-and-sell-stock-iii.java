805c901931eca40e5334f7da147c38f2e0494c20
Saurabh Kumar <saukum1999@gmail.com>
2022-09-08 23:25:26 +0530

    Time: 245 ms (14.56%), Space: 124.3 MB (25.31%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -3,15 +3,15 @@ class Solution {
         int len = prices.length;
         int[][][] dp = new int[len + 1][2][3];
         
-        for(int i = 0; i < len; i++){
-            for(int j = 0; j < 2; j++)
-                dp[i][j][0] = 0;
-        }
+//         for(int i = 0; i < len; i++){
+//             for(int j = 0; j < 2; j++)
+//                 dp[i][j][0] = 0;
+//         }
         
-        for(int i = 0; i < 2; i++){
-            for(int j = 0; j < 3; j++)
-                dp[len][i][j] = 0;
-        }
+//         for(int i = 0; i < 2; i++){
+//             for(int j = 0; j < 3; j++)
+//                 dp[len][i][j] = 0;
+        // }
 
         for (int i = len - 1; i >= 0; i--) {
             for (int buy = 0; buy <= 1; buy++) {
