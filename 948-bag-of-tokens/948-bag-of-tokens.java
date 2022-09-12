fca281e01fb9cd4934ab56baae3b8404e6528485
Saurabh Kumar <saukum1999@gmail.com>
2022-09-12 20:12:01 +0530

    Time: 5 ms (19.68%), Space: 42.8 MB (78.31%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.java
+++ 948-bag-of-tokens/948-bag-of-tokens.java
@@ -1,35 +1,62 @@
+// class Solution {
+//     public int bagOfTokensScore(int[] tokens, int power) {
+        
+//         if(tokens.length == 1){
+//             return (power / tokens[0]);
+//         }
+//         Arrays.sort(tokens);
+//         int l = 0;
+//         int r = tokens.length;
+//         int res = 0;
+//         int score = 0;
+        
+//         while(l<=r){
+//             if(power >= tokens[l]){
+//                 score++;
+//                 power -= tokens[l];
+//                 l++;
+//             }
+//             else if(power < tokens[l] && score > 0){
+//                 power += tokens[--r];
+//                 score--;
+//                 // r--;
+//             }
+//             else{
+//                 res = Math.max(res, score);
+//                 break;
+//             }
+//             System.out.println(res + " " + score);
+//             res = Math.max(res, score);
+//         }
+        
+//         return res;
+        
+//     }
+// }
+
 class Solution {
     public int bagOfTokensScore(int[] tokens, int power) {
         
-        if(tokens.length == 1){
-            return (power / tokens[0]);
-        }
         Arrays.sort(tokens);
-        int l = 0;
-        int r = tokens.length;
-        int res = 0;
-        int score = 0;
+        int res=0;
         
-        while(l<r){
-            if(power >= tokens[l]){
+        int l=0, r=tokens.length-1,score=0;
+        
+        while(l<=r){
+            if(power<tokens[l]){
+                if(score>0){
+                    power+=tokens[r--];
+                    score--;
+                }else{
+                    return res;
+                }
+            }else{
+                power-=tokens[l++];
                 score++;
-                power -= tokens[l];
-                l++;
+                res = Math.max(score,res);
             }
-            else if(power < tokens[l] && score > 0){
-                power += tokens[--r];
-                score--;
-                // r--;
-            }
-            else{
-                res = Math.max(res, score);
-                break;
-            }
-            System.out.println(res + " " + score);
-            res = Math.max(res, score);
         }
         
         return res;
-        
     }
 }
