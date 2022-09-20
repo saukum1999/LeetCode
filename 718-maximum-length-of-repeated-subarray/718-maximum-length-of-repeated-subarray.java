d807c607e1be6e0f1536d788b89948f392920960
Saurabh Kumar <saukum1999@gmail.com>
2022-09-20 22:37:37 +0530

    Time: 85 ms (49.15%), Space: 82 MB (17.14%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
@@ -1,6 +1,6 @@
 class Solution {
     public int findLength(int[] nums1, int[] nums2) {
-        int res = Integer.MIN_VALUE;
+        int res = 0;
         int l1 = nums1.length;
         int l2 = nums2.length;
         int[][] dp = new int[l1 + 1][l2 + 1];
@@ -12,11 +12,10 @@ class Solution {
         
         for(int i = 1; i <= l1; i++){
             for(int j = 1; j <= l2; j++){
-                if(nums1[i - 1] == nums2[j - 1])
+                if(nums1[i - 1] == nums2[j - 1]){
                     dp[i][j] = 1 + dp[i - 1][j - 1];
-                else
-                    dp[i][j] = 0;
-                res = Math.max(res, dp[i][j]);
+                    res = Math.max(res, dp[i][j]);
+                }
             }
         }
         
