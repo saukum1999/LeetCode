dca77d709bdb5cb0b18579bfd4a914c53cdba701
Saurabh Kumar <saukum1999@gmail.com>
2022-09-20 22:08:12 +0530

    Time: 13 ms (92.31%), Space: 46.1 MB (74.23%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -3,24 +3,27 @@ class Solution {
         
         int l1 = text1.length();
         int l2 = text2.length();
-        Integer[][] dp = new Integer[l1 + 1][l2 + 1];
+        int[][] dp = new int[l1 + 1][l2 + 1];
         
-        return helper(l1, l2, dp, text1, text2);
+        for(int i = 0; i <= l2; i++)
+            dp[0][i] = 0;
+        
+        for(int i = 0; i <= l1; i++)
+            dp[i][0] = 0;
+        
+        for(int i = 1; i <= l1; i++){
+            for(int j = 1; j <= l2; j++){
+                if(text1.charAt(i - 1) == text2.charAt(j - 1))
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        
+        return dp[l1][l2];
         
         
     }
     
-    public int helper(int i1, int i2, Integer[][] dp, String s1, String s2){
-        if(i1 == 0 || i2 == 0)
-            return 0;
-        
-        if(dp[i1][i2] != null)
-            return dp[i1][i2];
-        
-        if(s1.charAt(i1 - 1) == s2.charAt(i2 - 1))
-            dp[i1][i2] = 1 + helper(i1 - 1, i2 - 1, dp, s1, s2);
-        else
-            dp[i1][i2] = Math.max(helper(i1 - 1, i2, dp, s1, s2), helper(i1, i2 - 1, dp, s1, s2));
-        return dp[i1][i2];
-    }
+
 }
