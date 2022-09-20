691eab3c33d7cd3bd2116123dd07c1fc2aafbac7
Saurabh Kumar <saukum1999@gmail.com>
2022-09-20 22:34:41 +0530

    Time: 97 ms (33.13%), Space: 78.4 MB (43.62%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
@@ -5,10 +5,10 @@ class Solution {
         int l2 = nums2.length;
         int[][] dp = new int[l1 + 1][l2 + 1];
         
-        for(int i = 0; i <= l1; i++)
-            dp[i][0] = 0;
-        for(int i = 0; i <= l2; i++)
-            dp[0][i] = 0;
+        // for(int i = 0; i <= l1; i++)
+        //     dp[i][0] = 0;
+        // for(int i = 0; i <= l2; i++)
+        //     dp[0][i] = 0;
         
         for(int i = 1; i <= l1; i++){
             for(int j = 1; j <= l2; j++){
